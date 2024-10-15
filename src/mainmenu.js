675f9b1b21b7d92e2a2f7cc322f2a95ee5e40c66
SAFAWI <SAFAWI@DESKTOP-3LLQNMU>
2024-10-15 14:19:40 +0300

    first commit

---
+++ src/mainmenu.js
@@ -0,0 +1,38 @@
+class MainMenu {
+  constructor() {
+    const { Menu } = require("electron");
+    let template = [
+      {
+        label: "File",
+        submenu:[
+            {
+                label:"Close",
+                role: "quit"
+            }
+        ]
+      },
+      {
+        label: "Help",
+        click: ()=>{
+            const {shell}=require ('electron')
+            shell.openExternal("https://electronjs.org")
+        }
+      },
+      {
+        label: "About",
+        click: ()=>{
+            const {dialog}=require ('electron')
+            dialog.showMessageBox({
+                type:"info",
+                buttons:["OK"],
+                title: "About",
+                message:"Hello about"
+            })
+      },
+    }
+    ];
+    let menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+  }
+}
+module.exports = { MainMenu };
